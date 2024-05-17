8691107f391ce90f90621280c87282e656d1c1c9
Andrei Bituleanu <andrei.bituleanu@yahoo.com>
2024-05-17 00:28:25 -0400

    Finished.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 "use strict";
 
 const mainImage = document.getElementById("mainImage");
+const menu = document.getElementById("menu");
+const closeMenu = document.getElementById("closeButton");
+const opacityBody = document.getElementById("opacityBody");
+const sideBar = document.getElementById("sideBarMenu");
 
 function updateImage() {
 	if (window.innerWidth >= 700) {
@@ -10,6 +14,27 @@ function updateImage() {
 	}
 }
 
+function sideBarMenu() {
+	sideBar.style.transform = "translateX(0)";
+	opacityBody.style.opacity = "0.7";
+	sideBar.style.zIndex = "2";
+	opacityBody.style.zIndex = "1";
+
+	opacityBody.addEventListener("click", sideBarClose);
+}
+
+function sideBarClose() {
+	sideBar.style.transform = "translateX(100%)";
+	opacityBody.style.opacity = "0";
+
+	sideBar.style.zIndex = "0";
+	opacityBody.style.zIndex = "-1";
+
+	opacityBody.removeEventListener("click", sideBarClose);
+}
+
 updateImage();
 
+menu.addEventListener("click", sideBarMenu);
+closeMenu.addEventListener("click", sideBarClose);
 window.addEventListener("resize", updateImage);
